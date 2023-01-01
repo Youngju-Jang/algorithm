ca34d02a901c182dca66ce54dbd0c3f799275666
Youngju Jang <joj1043@nate.com>
2023-01-01 20:21:10 +0900

    [level 0] Title: 가까운 수, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array, int n) {
+          int gap = 101;
+          int answer = 300;
+          for(int i : array){
+               if(Math.abs(n-i) < gap){
+                    gap = Math.abs(n-i);
+                   answer = i;
+               }else if(Math.abs(n-i) == gap){
+                    answer = Math.min(answer, i);
+               }
+          }
+          return answer;
+     }
+}
