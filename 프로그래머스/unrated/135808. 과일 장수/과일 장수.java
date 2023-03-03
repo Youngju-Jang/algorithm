670f63eda32b9b1d2d39ccadb5ff7b0305a14750
Youngju Jang <joj1043@nate.com>
2023-03-03 13:10:46 +0900

    [unrated] Title: 과일 장수, Time: 2.26 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -4,12 +4,8 @@ class Solution {
     public int solution(int k, int m, int[] score) {
           int answer = 0;
           int[] sorted = Arrays.stream(score).sorted().toArray();
-          for(int i=sorted.length-1; i>sorted.length%m -1; i-=m){
-               int min = sorted[i];
-               for(int j=0; j<m; j++){
-                    min = Math.min(sorted[i - j], min);
-               }
-               answer += min * m;
+          for(int i=sorted.length; i>sorted.length%m; i-=m){
+               answer += sorted[i-m] * m;
           }
           return answer;
      }
