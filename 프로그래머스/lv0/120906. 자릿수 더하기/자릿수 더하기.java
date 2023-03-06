7fea89ca72ff37a2f1306db4c248a55bfc796bb3
Youngju Jang <joj1043@nate.com>
2023-03-06 21:14:01 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.16 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+          int answer = 0;
+          for(String s : String.valueOf(n).split("")){
+               answer += Integer.parseInt(s);
+          }
+          return answer;
+     }
+}
