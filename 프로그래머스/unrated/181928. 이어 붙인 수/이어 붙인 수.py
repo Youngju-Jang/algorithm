2ed5c900676549c67536259cf587c676792949df
Youngju Jang <joj1043@nate.com>
2023-07-24 08:25:43 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -1,5 +1,5 @@
 def solution(num_list):
     answer = 0
-    even = ''.join(map(str,[even for even in num_list if even%2 == 0]));
-    odd = ''.join(map(str,[even for even in num_list if even%2 == 1]));
+    even = ''.join([str(even) for even in num_list if even%2 == 0]);
+    odd = ''.join([str(odd) for odd in num_list if odd%2 == 1]);
     return int(even) + int(odd)
