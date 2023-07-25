4d655d0ce2a188197f81c805dde59a2346777e90
Youngju Jang <joj1043@nate.com>
2023-07-25 10:22:36 +0900

    [unrated] Title: 수 조작하기 2, Time: 11.59 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -2,10 +2,7 @@ def solution(numLog):
     dic = dict(zip([1, -1, 10, -10], ["w", "s", "d", "a"]));
     answer = ''
     pre = numLog[0]
-    for n in numLog :
-        if pre == n : 
-            continue
-        answer += dic[n-pre];
-        pre = n;
+    for i in range(1,len(numLog)):
+        answer += dic[numLog[i]-numLog[i-1]];
     
     return answer
