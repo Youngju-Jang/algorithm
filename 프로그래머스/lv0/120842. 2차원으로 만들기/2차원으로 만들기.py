5633534d3b53f10e17f819eb01b84e9fd771f620
Youngju Jang <joj1043@nate.com>
2023-08-22 10:01:27 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,15 @@
+import math
+
+def solution(num_list, n):    
+    l = len(num_list) #8
+    mox = math.ceil(l/n)
+    answer = [[0 for i in range(n)] for row in range(mox)]
+    namu = l%n
+    for i in range(mox):
+        for j in range(n):
+            if(n*i+j < l):
+                answer[i][j] = num_list[n*i+j]
+    
+    return answer
+
+          
